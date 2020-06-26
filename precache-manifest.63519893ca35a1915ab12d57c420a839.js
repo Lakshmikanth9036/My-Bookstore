749d93ea2c278a6316d97645258e3e33a73d753b
Lakshmikanth9036 <kanth9036.1997@gmail.com>
2020-06-26 22:53:22 +0530

    Updates

---
+++ precache-manifest.63519893ca35a1915ab12d57c420a839.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1fa2033ac1e49ccfa8ef63eaeb911f7",
+    "url": "/My-Bookstore/index.html"
+  },
+  {
+    "revision": "217e00c2ce57d7a67e92",
+    "url": "/My-Bookstore/static/css/main.ce5b6bb9.chunk.css"
+  },
+  {
+    "revision": "f64991e52c2f01497b80",
+    "url": "/My-Bookstore/static/js/2.02635490.chunk.js"
+  },
+  {
+    "revision": "455f9f3ae849b1b7c9d5b5f2d351830a",
+    "url": "/My-Bookstore/static/js/2.02635490.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9062e8392b845121bce8",
+    "url": "/My-Bookstore/static/js/3.ccc4beab.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/My-Bookstore/static/js/3.ccc4beab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14d5e4f88cac27cfa246",
+    "url": "/My-Bookstore/static/js/4.b4f6465d.chunk.js"
+  },
+  {
+    "revision": "217e00c2ce57d7a67e92",
+    "url": "/My-Bookstore/static/js/main.4165280a.chunk.js"
+  },
+  {
+    "revision": "fb76713777127c84779d",
+    "url": "/My-Bookstore/static/js/runtime-main.9b78945d.js"
+  },
+  {
+    "revision": "130aa5e872e6d30813daf06864e09a90",
+    "url": "/My-Bookstore/static/media/Image1.130aa5e8.png"
+  },
+  {
+    "revision": "03cc81c66e8246d4d483149686137be1",
+    "url": "/My-Bookstore/static/media/Image2.03cc81c6.png"
+  },
+  {
+    "revision": "cb9f9b8745d57ed2e7809f73e9833cb5",
+    "url": "/My-Bookstore/static/media/Image3.cb9f9b87.png"
+  },
+  {
+    "revision": "007be6315e0677ba843da1bef07cb401",
+    "url": "/My-Bookstore/static/media/Image4.007be631.png"
+  },
+  {
+    "revision": "0dcc78614d4ba10e99e644263bddd94f",
+    "url": "/My-Bookstore/static/media/Image5.0dcc7861.png"
+  },
+  {
+    "revision": "93bbf4b88366cb5502c0004b6ea4ea5c",
+    "url": "/My-Bookstore/static/media/Image6.93bbf4b8.png"
+  },
+  {
+    "revision": "030fface18689c1524e0e43e274fc8c4",
+    "url": "/My-Bookstore/static/media/Image7.030fface.png"
+  },
+  {
+    "revision": "cac5a909249f327c958088eacc6ae665",
+    "url": "/My-Bookstore/static/media/Image8.cac5a909.png"
+  },
+  {
+    "revision": "a6101cd4a82299b6546a0642bad64888",
+    "url": "/My-Bookstore/static/media/Image9.a6101cd4.png"
+  },
+  {
+    "revision": "73bc5c549b10c24da46369de75a49597",
+    "url": "/My-Bookstore/static/media/Success2x.73bc5c54.png"
+  },
+  {
+    "revision": "1e1bcf7ffee0362211c5796b3eb531bc",
+    "url": "/My-Bookstore/static/media/bookstore.1e1bcf7f.png"
+  },
+  {
+    "revision": "e077ebc98b55b536136871605a4a2714",
+    "url": "/My-Bookstore/static/media/forgot.e077ebc9.jpg"
+  },
+  {
+    "revision": "f99ede08089d3ce2a6e4d8d7cf3b2e52",
+    "url": "/My-Bookstore/static/media/ichigo.f99ede08.jpg"
+  },
+  {
+    "revision": "62946c01d32a9f5f6a91cdf45b9870d2",
+    "url": "/My-Bookstore/static/media/login.62946c01.jpg"
+  },
+  {
+    "revision": "74165250b756792d6f08d55412a645de",
+    "url": "/My-Bookstore/static/media/registration.74165250.jpg"
+  },
+  {
+    "revision": "2e3e061d901258aa0fe3ba666cbc2585",
+    "url": "/My-Bookstore/static/media/reset.2e3e061d.jpg"
+  },
+  {
+    "revision": "b6364ffa375154a866b02d753d5eaf9d",
+    "url": "/My-Bookstore/static/media/verify.b6364ffa.jpg"
+  }
+]);
